999d0dc806842994b69eb48551d6c798eecc04fd
Yulio Aldi Widargo <66007172+yulioaldiw@users.noreply.github.com>
2022-11-23 15:32:56 +0700

    third criteria done

--- src/handler.js
+++ src/handler.js
@@ -110,4 +110,26 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((n) => n.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
